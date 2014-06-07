26272a13a1f4a5e542f8d6480b6fcad61706301d
Clayton Rieck <cjrieck123@gmail.com>
2014-06-07 13:37:41 -0400

    manipulate DOM

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
 
+// var wordsToReplace = $('div:contains("I")');
+// $.each(wordsToReplace, function(index, value){
+// 	console.log("Hi");
+// 	value.replaceAll("<h1>You</h1>");
+// });
+
+$("body").append('<div>ahilfbelshvbaelihvb</div>');
+// console.log("HIIII");
